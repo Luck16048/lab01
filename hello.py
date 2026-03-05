d0c919cae4b5dbac7c649f25593e55e9fa17446b
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-03-05 12:10:22 +0100

    First commit

--- hello.py
+++ hello.py
@@ -7,6 +7,6 @@ name = "Tali"
 current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 version_info = sys.version
 
-print("Name: " + name)
+print("Name: " +  name)
 print("Current date: " + current_date)
 print("Version Python: " + version_info)
